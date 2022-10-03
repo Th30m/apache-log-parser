c410caea1272ff2df28e3cd23f447a70611cc315
Th30m <114905329+Th30m@users.noreply.github.com>
2022-10-03 09:27:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import re
+from pprint import pprint
+import json
+
+
+
+with open("./access.log", "r") as file_pointer:
+  lines = file_pointer.readlines()
+
+log_regex = r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[.+\] \"[^\"]+\" (\d{3}) \d+ \"\-\" \"(\w+[a-zA-Z0-9_])"
+
+access_data = []
+for line in lines:
+  data = re.findall(log_regex, line)[0]
+  access_data.append(data)
+
+ip_list = [ip for ip, status_code in access_data]
+
+#print(ip_list)
+
+ip_count = {}
+
+for ip in ip_list:
+  if ip not in ip_count:
+    ip_count[ip] = 1
+  else:
+    ip_count[ip] += 1
+#print(ip_count)
+
+final_data = {}
+
+for ip, status_code in access_data:
+  final_data[ip] = {}
+
+print(final_data)
+
+for ip, status_code in access_data:
+  if status_code not in final_data[ip]:
+    final_data[ip][status_code] = 1
+  else:
+    final_data[ip][status_code] += 1
+
+pprint(final_data)
+
+with open("access.json", "w") as json_file_pointer:
+  json.dump(final_data, json_file_pointer, indent=0)
